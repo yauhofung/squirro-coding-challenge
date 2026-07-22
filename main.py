336b6e6221fb16ee623f72f79556db75299545b5
Yau Ho Fung <54601881+yauhofung@users.noreply.github.com>
2026-07-22 11:10:22 -0400

    Add challenge PDF & skeleton script

---
+++ main.py
@@ -0,0 +1,80 @@
+import argparse
+import logging
+
+"""
+Skeleton for Squirro Delivery Hiring Coding Challenge
+November 2025
+"""
+
+
+log = logging.getLogger(__name__)
+
+
+class NYTimesSource(object):
+    """
+    A data loader plugin for the NY Times API.
+    """
+
+    def __init__(self):
+        pass
+
+    def connect(self, inc_column=None, max_inc_value=None):
+        """Connect to the source"""
+        log.debug("Incremental Column: %r", inc_column)
+        log.debug("Incremental Last Value: %r", max_inc_value)
+
+    def disconnect(self):
+        """Disconnect from the source."""
+        # Nothing to do
+        pass
+
+    def getDataBatch(self, batch_size):
+        """
+        Generator - Get data from source on batches.
+
+        :returns One list for each batch. Each of those is a list of
+                 dictionaries with the defined rows.
+        """
+        # TODO: implement - this dummy implementation returns one batch of data
+        yield [
+            {
+                "headline.main": "The main headline",
+                "_id": "1234",
+            }
+        ]
+
+    def getSchema(self):
+        """
+        Return the schema of the dataset
+        :returns a List containing the names of the columns retrieved from the
+        source
+        """
+
+        schema = [
+            "title",
+            "body",
+            "created_at",
+            "id",
+            "summary",
+            "abstract",
+            "keywords",
+        ]
+
+        return schema
+
+
+if __name__ == "__main__":
+    config = {
+        "api_key": "NYTIMES_API_KEY",
+        "query": "Silicon Valley",
+    }
+    source = NYTimesSource()
+
+    # This looks like an argparse dependency - but the Namespace class is just
+    # a simple way to create an object holding attributes.
+    source.args = argparse.Namespace(**config)
+
+    for idx, batch in enumerate(source.getDataBatch(10)):
+        print(f"{idx} Batch of {len(batch)} items")
+        for item in batch:
+            print(f"  - {item['_id']} - {item['headline.main']}")
